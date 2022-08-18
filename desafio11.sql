66664cc349d64ff2b3cfbec04608ec9b2cf16462
jessica <jmillene12@gmail.com>
2022-08-18 15:27:59 -0300

    requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+	cancao AS nome_musica,
+    CASE 
+		WHEN  cancao LIKE '%Amar%' THEN REPLACE(cancao,'Amar','Code Review') 
+        WHEN cancao LIKE '%SUPERSTAR%' THEN REPLACE(cancao,'SUPERSTAR', 'SUPERDEV')
+        WHEN cancao LIKE '%Bard%' THEN REPLACE(cancao, 'Bard', 'QA')
+        WHEN cancao LIKE '%SOUL%' THEN REPLACE(cancao, 'SOUL', 'CODE')
+        WHEN cancao LIKE '%Pais%' THEN REPLACE(cancao, 'Pais', 'Pull Requests')
+	END AS novo_nome
+FROM SpotifyClone.cancao as ca
+ORDER BY novo_nome DESC
+LIMIT 5;
