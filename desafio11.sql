87f3b80842ebe842204e052e350c5aeb6941025a
jessica <jmillene12@gmail.com>
2022-08-18 17:10:13 -0300

    teste avaliador

--- desafio11.sql
+++ desafio11.sql
@@ -7,6 +7,6 @@ SELECT
         WHEN cancao LIKE '%SOUL%' THEN REPLACE(cancao, 'SOUL', 'CODE')
         WHEN cancao LIKE '%Pais%' THEN REPLACE(cancao, 'Pais', 'Pull Requests')
 	END AS novo_nome
-FROM SpotifyClone.cancao as ca
-ORDER BY novo_nome DESC
-LIMIT 5;
+  FROM SpotifyClone.cancao as ca
+  ORDER BY novo_nome DESC
+  LIMIT 5;
